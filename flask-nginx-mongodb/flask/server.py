71cc33d5184258b8f5a2b2b597af65a7896b9161
dinomite505 <dino.milje@gmail.com>
2023-10-28 17:48:26 +0200

    Routing to flask app

--- flask-nginx-mongodb/flask/server.py
+++ flask-nginx-mongodb/flask/server.py
@@ -10,4 +10,12 @@ app = Flask(__name__)
 # Create connection to a MongoDB server, hostname:port
 client = MongoClient("mongo:207017")
 
-
+# Defining a route to Flask app
+@app.route('/')
+# From root URL execute todo
+def todo():
+    try:
+        client.admin('ismaster')
+    except:
+        return: "Server is not available"
+    return "Hello from MongoDB client"
